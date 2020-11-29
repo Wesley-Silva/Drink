ce4200ae95d0beb01cf2100b9c369af9df2849fb
Wesley Silva <wesleybf3@gmail.com>
2020-11-28 21:56:04 -0300

    Exibir o troco a cada item comprado

--- index.js
+++ index.js
@@ -5,10 +5,12 @@
 
 const REFRIGERANTES = [4];
 const valor = [7];
-const TROCO = [7];
+var TROCO = [7];
 
 var saldo = 0;
 var opcao = 1;
+var troco = 0;
+var cont = 0;
 
 REFRIGERANTES[0] = 3.50;
 REFRIGERANTES[1] = 4.80;
@@ -57,6 +59,8 @@ var total = 0;
             if (event.target.parentElement.classList.contains('buttons')) {
                 //console.log(event.target.parentElement.previousElementSibling.src);
                 //console.log(event.target.parentElement.previousElementSibling.textContent);              
+               
+
                 const item = {};
 
                 var price = event.target.parentElement.previousElementSibling.textContent;  
@@ -78,8 +82,10 @@ var total = 0;
                 if (totalAtual < finalPriceConvertido) {
                     alert('Saldo insuficiente, deposite o dinheiro');
                     return false;                    
-                }
-
+                }else{
+                    document.getElementById('finalSquare').innerHTML = '';
+                    
+                            
 
                 document.getElementById('totalCompra').textContent = totalComprado;
 
@@ -99,25 +105,62 @@ var total = 0;
                 const cart = document.getElementById('cart2');
                 const total = document.querySelector('.cart-total-container');                
                 cart.insertBefore(gridItem, total);  
-                
-                /*var finalPriceConvertido  = parseFloat(finalPrice);
-                let saldoCompra = document.getElementById('totalCompra').textContent;
-                var saldoCompraConvertido  = parseFloat(saldoCompra);
-                let totalComprado2 = saldoCompraConvertido + finalPriceConvertido;
-                               
-                document.getElementById('totalCompra').textContent = totalComprado2;*/
 
-                //let totalAtual = document.getElementById('totalGeral').textContent;
-                // var saldoAtualizado = finalPriceConvertido - totalAtual;
                 
-                // let saldoAtualizadoConvertido = Math.abs(saldoAtualizado)
+                troco = saldoAtualizadoConvertido; 
+                cont = 6;
 
-                /*if (totalAtual < finalPriceConvertido) {
-                    alert('Saldo insuficiente, deposite o dinheiro');
-                    return false;                    
-                }*/
-                //document.getElementById('totalGeral').textContent = saldoAtualizadoConvertido.toFixed(2);
+                while (cont >= 0) {
+                    TROCO[cont] = 0;
+                    while (troco >= valor[cont]) {
+                        TROCO[cont] = TROCO[cont] + 1;
+                        troco = troco - valor[cont];
+                    }
+                    cont = cont - 1;
+                }
+                
+                //document.getElementById('finalSquare').style.display = 'none';
+                
+                cont = 0;
+                for (let index = 0; TROCO[index] < 6; index++) {
+
+                    const gridItem1 = document.createElement('div')   
+                    gridItem1.className = "square";             
+                    gridItem1.innerHTML = `                  
+                            <div class="block">
+                                <div class="centered" id="trocoPosicao">
+                                    ${ TROCO[index] }
+                                </div>                            
+                            </div>
+                    `;
+                    //let ret = document.getElementsByClassName('centered');
+                    let ret = document.getElementsByClassName('finalSquare');
+                    console.log(ret)
+
+                    //const cart1 = document.getElementById('trocoPosicao');
+                    const total1 = document.querySelector('.teste');   
+                    
+                    ret[0].insertBefore(gridItem1, total1);
+                   
+
+                    
+
+                    //ret.insertAdjacentElement('afterend', gridItem1);
+
+
+                    // const gridItem1 = document.createElement('div')                
+                    // gridItem1.innerHTML = `                  
+                    //         <span>${ TROCO[index] }</span>
+                    // `;
+                    // const cart1 = document.getElementById('trocoPosicao');
+                    // const total1 = document.querySelector('.teste');                
+                    // cart1.insertBefore(gridItem1, total1);
+                }
+                
+                saldo = 0; 
             }
+            }
+            //break;
         })
     })
 })();
